6c7df27487572aebea1219f4247ced8e5c1a76e4
shykuopo <Sam82267535@yahoo.com.tw>
2018-11-28 23:41:14 +0800

    update adjustment

--- app/application/forms/article_request.rb
+++ app/application/forms/article_request.rb
@@ -6,7 +6,7 @@ module SeoAssistant
   module Forms
     ArticleRequest = Dry::Validation.Params do
       # :article should be the same as front_end's input name 
-      required(:article).filled(:str?)
+      required(:article).filled
 
       configure do
         config.messages_file = File.join(__dir__, 'errors/article_request.yml')
